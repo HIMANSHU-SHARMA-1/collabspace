25f968a84d7712d62e2410c11c102b53b3af0e2b
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-14 19:14:50 +0530

    Wire up AI routes

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const aiController = require('../controllers/aiController')
+const auth = require('../middleware/auth')
 
 router.get('/test',aiController.testAi)
+router.get('/recommend-projects', auth, aiController.recommendProjects)
 module.exports = router
