c63d1edd06b4adf6c6b7f248c52ddf232ae4ea5a
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-15 00:06:52 +0530

    checking and printing logs for late response time

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -17,10 +17,14 @@ const testAi = async(req,res)=>{
 
 const recommendProjects = async(req,res)=>{
    try{
+    console.time("Find User");
     const user = await User.findById(req.user.id)
+    console.timeEnd("Find User");
     if(!user){
         return res.status(404).json({success:false,message:'User not found'})
     }
+
+    console.time("Find Projects");
     const project = await Project.find(
         {
             status:'open',
@@ -28,13 +32,17 @@ const recommendProjects = async(req,res)=>{
             members:{$ne:req.user.id}
         }
     )
+    console.timeEnd("Find Projects");
+
     if(!project.length === 0){
         return res.status(404).json({message:'No open project for you to join now'})
     }
+    console.time("AI Recommendation");
     const recommendation = await getProjectRecommendations(
         user.skills,
         project
     )
+    console.timeEnd("AI Recommendation");
     res.status(200).json({sucess:true, data:recommendation})
    }
    catch(err){
