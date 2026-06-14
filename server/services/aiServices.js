e6991ee4a7c736b7660b735e5b139d69cb8ef92a
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-14 23:51:16 +0530

    Update AI service logic

--- server/services/aiServices.js
+++ server/services/aiServices.js
@@ -1,28 +1,67 @@
-const openAi = require('openai')
+const openAi = require('openai');
 
 const client = new openAi({
     baseURL:'https://integrate.api.nvidia.com/v1',
     apiKey:process.env.NVIDIA_API_KEY
 });
 
-const testAIConnection = async()=>{
+const callAi = async(prompt)=>{
+    console.time("AI Request");
     const completion = await client.chat.completions.create({
         model:'deepseek-ai/deepseek-v4-flash',
-        messages: [
+        messages:[
             {
                 role:"user",
-                content:"Reply with only: AI connection working"}
+                content:prompt
+            }
         ],
-        temperature:0
+        temperature:0,
+        max_tokens: 500
     });
+    console.timeEnd("AI Request");
     return completion.choices[0].message.content;
+}
+
+const testAIConnection = async()=>{
+    const prompt = 'Reply with only: AI connection working'
+    
+    return await callAi(prompt);
 
 };
 
 const getProjectRecommendations = async(userskills, projects)=>{
-    console.log(userskills)
-console.log(projects)
-    return 'working'
+
+    const skillText = userskills.map(
+        skills => `${skills.name} (${skills.rating}/5)`).join(',');
+
+    const projectText = projects.map(
+        project =>`Project Name: ${project.projectname}
+        Required Skill: ${project.requiredSkill.join(',')}
+        Description: ${project.description}`).join('\n')  
+        
+        
+    const prompt = `You are AI project recommendation engine.
+    
+    User Skills: ${skillText}
+
+    Available Projects: ${projectText}
+
+    Rank projects from best match to worst match.
+
+    Return only valid json in this format:
+
+    [
+        {
+            "projectName":"Project Name",
+            "score":95,
+            "reason":"Reason for recommendation"
+        }
+    ]
+    `;   
+//     console.log(prompt);
+// console.log(prompt.length);
+     const response = await callAi(prompt);   
+     return response;
 }
 
 module.exports = {testAIConnection, getProjectRecommendations}
