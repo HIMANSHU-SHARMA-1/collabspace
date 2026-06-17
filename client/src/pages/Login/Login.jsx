28b74d544c3beeb6acfb217005f6b551f747312b
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-17 21:09:29 +0530

    Implement login form with auth API integration

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useState } from 'react'
+import {login} from '../../services/authService'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Login = () => {
+const [formData, setformData] = useState({})
+const navigate = useNavigate() //redirect after after submit
+
+const handleSubmit = async (e)=>{
+  e.preventDefault()
+  try{
+    const data = await login(formData.email, formData.password)
+    localStorage.setItem('token',data.token)
+    if(data.success === true){
+      navigate("/dashboard")
+    }
+  }
+  catch(err){
+    console.log(err.message)
+  }
+  finally{
+    e.target.reset()
+  }
+ 
+// console.log(formData)
+
+}
+
+const addData = (e)=>{
+  setformData({...formData, [e.target.name]:e.target.value})
+}
   return (
-    <div>Login</div>
+    <>
+    <form action="" onSubmit={(e)=>{handleSubmit(e)}}>
+    <input type="text" name='email'  onInput={(e)=>{addData(e)}} placeholder='enter your email'/>
+    <input type='password' name='password'  onInput={(e)=>{addData(e)}} placeholder='enter your password'/>
+    <button type='submit'>Login</button>
+    </form>
+    </>
   )
 }
 
