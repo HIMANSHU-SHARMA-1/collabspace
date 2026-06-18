de8ec413c6226c7a11523654ea2d18495334cac7
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-18 20:48:03 +0530

    Wrap routes with ProtectedRoute and PublicRoute guards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { Routes,Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import Dashboard from './pages/Dashboard/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import PublicRoute from './components/PublicRoute/PublicRoute'
 
 const App = () => {
   return (
     <>
          <Routes>
-          <Route path='/' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/' element={<PublicRoute><Login/></PublicRoute>}/>
+          <Route path='/register' element={<PublicRoute><Register/></PublicRoute>}/>
+          <Route path='/dashboard' element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
          </Routes>
     </>
   )
