1eb3eb83e942e63a4bc108b030ba02c3b2410427
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-20 20:11:38 +0530

    Validate JWT expiry in ProtectedRoute

--- client/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,31 @@
-import { Navigate } from "react-router-dom"
+import { Navigate } from "react-router-dom";
+import {jwtDecode}  from "jwt-decode";
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/" />;
+  } 
 
-    const token = localStorage.getItem('token')
-    if(!token){
-        return <Navigate to='/'/>
+    let payload;
+    try {
+      payload = jwtDecode(token);
+    } catch (err) {
+      //token is not a valid JWT
+      payload = null;
     }
-  return (children)
-}
+    if (payload !== null) {
+      const isExpired = payload.exp * 1000 < Date.now();
+      if (isExpired) {
+        alert("Your Session has expired. Please Login again!");
+        return <Navigate to="/" />;
+      } else {
+        return children;
+      }
+    } else {
+      alert("Invalid session Please Log in Again!");
+      return <Navigate to="/" />;
+    }
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
