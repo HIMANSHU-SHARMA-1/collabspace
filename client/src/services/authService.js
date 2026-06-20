ac13824f2d9888a63d3519573410997960afc729
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-20 20:10:28 +0530

    Add register function to auth service

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -5,12 +5,20 @@ const login = async(email, password)=>{
 try{
     const res = await api.post('/api/auth/login',{email, password})
     return res.data
-
-
 }
 catch(err){
     throw err;
+}
+}
+
+const register = async(userData)=>{
+    try{
+        const res = await api.post('/api/auth/register',userData)
+        return res.data
+    }
+    catch(err){
+        throw err
+    }
 
 }
-}
-export {login}
+export {login, register}
