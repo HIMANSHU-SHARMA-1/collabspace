26379ab6c479d8be71863e01ac7202adf9e7dc56
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-22 20:58:32 +0530

    fix: Public route token bugs

--- client/src/components/PublicRoute/PublicRoute.jsx
+++ client/src/components/PublicRoute/PublicRoute.jsx
@@ -1,7 +1,9 @@
 import { Navigate } from "react-router-dom"
+import { useAuth } from "../../context/AuthContext"
+
 
 const PublicRoute = ({children}) => {
-    const token = localStorage.getItem('token')
+  const{token}= useAuth()
     if(token){
         return <Navigate to='/dashboard'/>
     }
