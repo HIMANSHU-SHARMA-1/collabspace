d5788d572d1284fe5a8917073bef8bf85dca8ca8
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-22 21:01:07 +0530

    used Authcontext destructuring

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,42 +1,62 @@
-import { useState } from 'react'
-import {login} from '../../services/authService'
-import { useNavigate } from 'react-router-dom'
-
-
+import { useState } from "react";
+import { login } from "../../services/authService";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 const Login = () => {
-const [formData, setformData] = useState({})
-const navigate = useNavigate() //redirect after after submit
+  const [formData, setformData] = useState({});
+  const navigate = useNavigate(); //redirect after after submit
+  const { setToken } = useAuth();
 
-const handleSubmit = async (e)=>{
-  e.preventDefault()
-  try{
-    const data = await login(formData.email, formData.password)
-    localStorage.setItem('token',data.token)
-    if(data.success === true){
-      navigate("/dashboard")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await login(formData.email, formData.password);
+      if (data.success === true) {
+        localStorage.setItem("token", data.token);
+        setToken(data.token);
+        navigate("/dashboard");
+      }
+    } catch (err) {
+      console.log(err.message);
     }
-  }
-  catch(err){
-    console.log(err.message)
-  }
- 
-// console.log(formData)
 
-}
+    // console.log(formData)
+  };
 
-const addData = (e)=>{
-  setformData({...formData, [e.target.name]:e.target.value})
-}
+  const addData = (e) => {
+    setformData({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
     <>
-    <form action="" onSubmit={(e)=>{handleSubmit(e)}}>
-    <input type="text" name='email'  onChange={(e)=>{addData(e)}} placeholder='enter your email' required/>
-    <input type='password' name='password'  onChange={(e)=>{addData(e)}} placeholder='enter your password' required/>
-    <button type='submit'>Login</button>
-    </form>
+      <form
+        action=""
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          type="text"
+          name="email"
+          onChange={(e) => {
+            addData(e);
+          }}
+          placeholder="enter your email"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => {
+            addData(e);
+          }}
+          placeholder="enter your password"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
