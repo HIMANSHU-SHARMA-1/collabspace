d83275df8a1f115bed497ace13047f1bfbebcd49
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-23 09:56:11 +0530

    updated Authcontext

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useContext, useState } from 'react'
+
+
+const AuthContext= createContext()
+
+const AuthProvider = ({children}) => {
+const [token, setToken] = useState(localStorage.getItem('token'))
+const [user, setUser] = useState({})
+
+  return (
+    <AuthContext.Provider value={{user, setUser, token, setToken}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+export const useAuth = ()=>useContext(AuthContext)
+export default AuthProvider
