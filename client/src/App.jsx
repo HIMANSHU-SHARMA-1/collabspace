453cb9876b1b830b3b0999e0ec73a86b4b789308
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-06-24 19:23:55 +0530

    Fixed Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,16 @@ import Register from './pages/Register/Register'
 import Dashboard from './pages/Dashboard/Dashboard'
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 import PublicRoute from './components/PublicRoute/PublicRoute'
-
+import CreateProject from './pages/Projects/CreateProject'
 const App = () => {
   return (
     <>
          <Routes>
-          <Route path='/' element={<PublicRoute><Login/></PublicRoute>}/>
-          <Route path='/register' element={<PublicRoute><Register/></PublicRoute>}/>
-          <Route path='/dashboard' element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
-         </Routes>
+  <Route path='/' element={<PublicRoute><Login/></PublicRoute>}/>
+  <Route path='/register' element={<PublicRoute><Register/></PublicRoute>}/>
+  <Route path='/dashboard' element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+  <Route path='/create-project' element={<ProtectedRoute><CreateProject/></ProtectedRoute>}/>
+</Routes>
     </>
   )
 }
