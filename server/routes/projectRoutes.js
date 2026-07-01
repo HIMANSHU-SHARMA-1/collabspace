e8c906558cc92c3c13f2e956e0a4d565cf6a51b9
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-07-01 19:54:52 +0530

    fix: add auth middleware to update and delete project routes

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -8,7 +8,7 @@ router.get('/getAll', projectController.getAllProjects)
 router.get('/myProjects',auth,projectController.getMyProjects)
 router.get('/By/:id',projectController.getProjectbyID)
 router.get('/joinedProjects',auth, projectController.getJoinedProjects)
-router.put('/update/:id', projectController.updateProjects)
-router.delete('/delete/:id', projectController.deleteProjects)
+router.put('/update/:id',auth, projectController.updateProjects)
+router.delete('/delete/:id',auth, projectController.deleteProjects)
 
 module.exports = router;
