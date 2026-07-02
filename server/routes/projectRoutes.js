df5b738611792beea7db8744d5f5fe7766d6db31
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-07-02 21:01:18 +0530

    feat: add DELETE /removeMember/:projectId/:memberId route

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -9,6 +9,7 @@ router.get('/myProjects',auth,projectController.getMyProjects)
 router.get('/By/:id',projectController.getProjectbyID)
 router.get('/joinedProjects',auth, projectController.getJoinedProjects)
 router.put('/update/:id',auth, projectController.updateProjects)
+router.delete('/removeMember/:projectId/:memberId',auth, projectController.removeMember)
 router.delete('/delete/:id',auth, projectController.deleteProjects)
 
 module.exports = router;
