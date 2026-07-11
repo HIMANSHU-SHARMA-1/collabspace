43b9030e6f6ee3296f7936988c19ff6e63ec0528
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-07-11 11:44:22 +0530

    test: add test case for malformed jwt token in auth middleware

--- server/test/authMiddleware.test.js
+++ server/test/authMiddleware.test.js
@@ -4,6 +4,7 @@ const app = require('../index')
 
 describe('auth Middleware',()=>{
 
+    //without token test
     test('rejects a request with no Authorization header',async()=>{
         //Act
         const res = await request(app).get('/api/project/myProjects')
@@ -12,5 +13,18 @@ describe('auth Middleware',()=>{
         expect(res.body.success).toBe(false)
         expect(res.body.message).toBe('login required')
     })
+
+    //malformed token
+    test('reject a request with malformed token',async()=>{
+        //act
+        const res = await request(app).get('/api/project/myProjects').set('Authorization', 'Bearer this-is-not-a-real-token')
+
+        //Assert
+        expect(res.body.message).toBe('jwt malformed')
+
+
+
+    })
+
 
 })
