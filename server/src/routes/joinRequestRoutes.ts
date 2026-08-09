62eb8ac8d1885f602d463128f17ad02d12604afc
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-08-09 10:12:24 +0530

    refactor(ts): convert joinRequestRoutes to TypeScript

--- server/src/routes/joinRequestRoutes.ts
+++ server/src/routes/joinRequestRoutes.ts
@@ -1,12 +1,12 @@
-const express = require('express')
+import express = require('express')
 const router = express.Router()
 
-const joinRequestController = require('../controllers/joinRequestController')
-const auth = require('../middleware/auth')
+import joinRequestController = require('../controllers/joinRequestController')
+import auth = require('../middleware/auth')
 
 router.post('/send', auth, joinRequestController.sendRequest)
 router.put('/approve/:id', auth, joinRequestController.approveRequest)
 router.put('/reject/:id', auth, joinRequestController.rejectRequest)
 router.get('/all/:projectId',auth, joinRequestController.getAllRequest)
 
-module.exports = router;
+export = router;
