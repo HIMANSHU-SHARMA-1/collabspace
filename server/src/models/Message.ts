874e34b2a5d919b71b5b592ca9912b793d8eec79
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2026-08-09 09:44:31 +0530

    fix(ts): use lowercase string primitive instead of String wrapper in IMessage

--- server/src/models/Message.ts
+++ server/src/models/Message.ts
@@ -4,13 +4,13 @@ import mongoose = require('mongoose')
 interface IMessage extends mongoose.Document {
     sender:mongoose.Types.ObjectId,
     project:mongoose.Types.ObjectId,
-    content:String
+    content:string
 }
 
-const messageSchema = new mongoose.Schema<IMessage  >({
+const messageSchema = new mongoose.Schema<IMessage>({
     sender:{type:mongoose.Schema.Types.ObjectId, ref:'User',required:true},
     project:{type:mongoose.Schema.Types.ObjectId, ref:'Project', required:true},
-    content:{type:String}
+    content:{type:String, required:true}
 },{timestamps:true})
 
 const Message = mongoose.model<IMessage>('Message', messageSchema)
